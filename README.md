ad00bc7733e15b3a9b8323dc318c24f5ab576108
Sơn Trần-Nguyễn <github@sntran.com>
2024-04-29 10:23:11 -0500

    docs: types and examples

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # HTMLRewriter
+
+Provides the same HTMLRewriter for all platforms.
+
+## Examples
+
+Rewrite URL links in HTML:
+
+```js
+import { HTMLRewriter } from "@sntran/html-rewriter";
+
+export default {
+  async fetch(request) {
+    const OLD_URL = "developer.mozilla.org";
+    const NEW_URL = "mynewdomain.com";
+
+    class AttributeRewriter {
+      constructor(attributeName) {
+        this.attributeName = attributeName;
+      }
+      element(element) {
+        const attribute = element.getAttribute(this.attributeName);
+        if (attribute) {
+          element.setAttribute(
+            this.attributeName,
+            attribute.replace(OLD_URL, NEW_URL)
+          );
+        }
+      }
+    }
+
+    const rewriter = new HTMLRewriter()
+      .on("a", new AttributeRewriter("href"))
+      .on("img", new AttributeRewriter("src"));
+
+    const res = await fetch(request);
+    const contentType = res.headers.get("Content-Type");
+
+    // If the response is HTML, it can be transformed with
+    // HTMLRewriter -- otherwise, it should pass through
+    if (contentType.startsWith("text/html")) {
+      return rewriter.transform(res);
+    } else {
+      return res;
+    }
+  },
+};
+```
---
+++ mod.ts
@@ -0,0 +1,76 @@
+import {
+  DocumentHandlers, ElementHandlers,
+  HTMLRewriter as BaseHTMLRewriter,
+} from "npm:html-rewriter-wasm@0.4.1";
+
+export class HTMLRewriter {
+  #elementHandlers: Array<[string, ElementHandlers]> = [];
+  #documentHandlers: Array<DocumentHandlers> = [];
+
+  constructor() {}
+
+  /**
+   * Attaches a handler to the document
+   * @param {DocumentHandlers} handlers
+   * @returns {this}
+   */
+  onDocument(handlers: DocumentHandlers) {
+    this.#documentHandlers.push(handlers);
+    return this;
+  }
+
+  /**
+   * Attaches a handler to an element matching the selector
+   * @param {string} selector CSS selector to match on
+   * @param {ElementHandlers} handlers
+   * @returns {this}
+   */
+  on(selector: string, handlers: ElementHandlers) {
+    this.#elementHandlers.push([selector, handlers]);
+    return this;
+  }
+
+  /**
+   * Transforms the response body using the attached handlers
+   * @param {Response} response
+   * @returns {Response}
+   */
+  transform(response: Response) {
+    const body = response.body;
+    if (!body) {
+      return response;
+    }
+
+    const elementHandlers = this.#elementHandlers;
+    const documentHandlers = this.#documentHandlers;
+    let rewriter: BaseHTMLRewriter;
+
+    const { readable, writable } = new TransformStream({
+      start(controller) {
+        rewriter = new BaseHTMLRewriter((chunk) => {
+          if (chunk.length !== 0) {
+            controller.enqueue(chunk);
+          }
+        });
+
+        for (const [selector, handlers] of elementHandlers) {
+          rewriter.on(selector, handlers);
+        }
+        for (const handlers of documentHandlers) {
+          rewriter.onDocument(handlers);
+        }
+      },
+
+      transform: (chunk) => rewriter.write(chunk),
+      flush: () => rewriter.end(),
+    });
+
+    const promise = response.body.pipeTo(writable);
+    promise.catch(() => {
+    }).finally(() => rewriter.free());
+
+    return new Response(readable, {
+      headers: { "Content-Type": "text/html" },
+    });
+  }
+}
